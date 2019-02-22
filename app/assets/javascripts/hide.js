2b33857a2ba31ac5b22a4ae53c984096b84c4e2c
Samuel Cornet <samuel@noukod.com>
2019-02-22 20:27:52 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,13 +1,8 @@
-// 
-// function hideWhenClicked(event) {
-//   const $hidden = $(event.target)
-//   $hidden.hide()
-//   debugger
-// }
-//
-// $('#hide_this').click(hideWhenClicked)
 
-
-function hideWhenClicked(){
-  document.getElementById("hide_this").style.display="none";
+function hideWhenClicked(event) {
+  const $hidden = $(event.target)
+  $hidden.hide()
+  debugger
 }
+
+$('#hide_this').click(hideWhenClicked)
